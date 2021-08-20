f408fa345f0c328bd96e9b4a565e08eda03e27e0
renat <khatymovrenat@gmail.com>
2021-08-20 19:04:40 +0300

    feature. Filter history

--- data_visualization.py
+++ data_visualization.py
@@ -3,6 +3,7 @@ from matplotlib.widgets import Slider, Button
 
 import random
 from matplotlib.animation import FuncAnimation
+import copy
 
 class Object:
     def __init__(self, x, y, time, id, is_global):
@@ -33,13 +34,21 @@ t_0 = time_ms
 
 is_global_obj = False
 
-for i in range(100, 1, -1):
+for i in range(100, 40, -1):
     time_ms += 100
     if i < 70:
         is_global_obj = True
     objects.append(Object(i, 0, time_ms, 1, is_global_obj))
-    # objects.append(Object(random.randrange(0, 100), random.randrange(-10, 10), time_ms, 1, False))
+    objects.append(Object(i - 20, -10, time_ms, 2, is_global_obj))
 
+is_global_obj = False
+
+for i in range(140, 100, -1):
+    time_ms += 100
+    if i < 20:
+        is_global_obj = True
+    objects.append(Object(i, 0, time_ms, 3, is_global_obj))
+    objects.append(Object(i - 20, -10, time_ms, 4, is_global_obj))
 
 packs = []
 cur_time = objects[0].time
@@ -61,9 +70,9 @@ fig, ax = plt.subplots()
 plt.subplots_adjust(bottom=0.35)
 plt.axis([-25, 25, -1, 150])
 
-pnts_global, = plt.plot([], [], "-bo")
-# text_g = plt.text(0, 0, "")
-pnts_candidate, = plt.plot([], [], "-ro", mfc='none')
+pnts_global, = plt.plot([], [], "bo")
+
+pnts_candidate, = plt.plot([], [], "ro", mfc='none')
 
 axSlider = plt.axes([0.1, 0.2, 0.8, 0.05])
 
@@ -73,6 +82,22 @@ sldr = Slider(ax=axSlider,
               valmax=time_ms,
               valstep=1,
               color="green")
+
+def filter_history(ids, history):
+    new_history = copy.deepcopy(history)
+    for pack in new_history:
+        i = 0
+        del_index = []
+        for obj in pack.objects:
+            if obj.id not in ids:
+                del_index.append(i)
+
+            i += 1
+
+        for j in range(len(del_index), 0, -1):
+            pack.objects.pop(j - 1)
+
+    return new_history
 
 def update_data(val):
     t_prev = t_0
@@ -86,13 +111,18 @@ def update_data(val):
         history_packs.append(pack_)
         t_prev = pack_.timestamp
 
+    ids = []
+    for obj in cur_pack.objects:
+        ids.append(obj.id)
+
+    new_history_packs = filter_history(ids, history_packs)
 
     pnts_g_x = []
     pnts_g_y = []
     pnts_c_x = []
     pnts_c_y = []
 
-    for history_pack in history_packs:
+    for history_pack in new_history_packs:
         for obj_plot in history_pack.objects:
             if (obj_plot.is_global):
                 pnts_g_x.append(obj_plot.y)
@@ -106,7 +136,6 @@ def update_data(val):
 
     plt.draw()
 
-    return pnts_global
 
 sldr.on_changed(update_data)
 
